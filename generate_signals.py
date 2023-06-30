ba970849bdd4be806eef541b0e002d9738cbaf69
mikasoderstrom <107407666+mikasoderstrom@users.noreply.github.com>
2023-06-30 08:25:37 +0200

    generate signals, need update of config
    The following parameters are needed in the config file:
    
    # --- EMULATING DATA variables ---
    f_sampling = 48828.125          # sampling frequency in Hz
    samples = 512               # number of samples to generate
    t_start = 0                 # start time of simulation 
    t_end = samples/f_sampling  # end time of simulation
    
    # Source variables
    away_distance = 700         # distance between the array and sources
    # source1
    f_start1 = 3000              # lowest frequency that the source emitts
    f_end1 = 3500                # highest frequency that the source emitts
    f_res1 = 20                 # resolution of frequency
    theta_deg1 = 40             # theta angel of source placement, relative to origin of array
    phi_deg1 = 0               # phi angel of source placement, relative to origin of array
    t_start1 = 0                # start time of emission
    t_end1 = t_end       # end time of emission
    
    # source1
    f_start2 = 500              # lowest frequency that the source emitts
    f_end2 = 550                # highest frequency that the source emitts
    f_res2 = 20                 # resolution of frequency
    theta_deg2 = 40             # theta angel of source placement, relative to origin of array
    phi_deg2 = -20              # phi angel of source placement, relative to origin of array
    t_start2 = 0                # start time of emission
    t_end2 = t_end              # end time of emission

---
+++ generate_signals.py
@@ -0,0 +1,99 @@
+# AUDIO SIGNALS
+import numpy as np
+import matplotlib.pyplot as plt
+from Audio_data import Audio_data
+from Audio_source import Audio_source
+import math
+
+import config
+
+def generate_filename():
+    if config.sources == 1:
+        filename ='emul_'+ 'samples='+str(config.samples) + '_'+ str(config.f_start1)+'Hz_'+'theta='+str(config.theta_deg1)+'_phi='+str(config.phi_deg1)+ \
+            '_A'+str(config.active_arrays)
+    elif config.sources == 2:
+        filename ='emul_'+'samples='+str(config.samples) + '_'+str(config.f_start1)+str(config.f_start2)+'Hz_'+'theta='+str(config.theta_deg1)+str(config.theta_deg2)+ \
+        '_phi='+str(config.phi_deg1)+str(config.phi_deg2)+'_A'+str(config.active_arrays)
+    return filename
+
+def r_vec(theta,phi):
+    r = np.array([(math.sin(theta)*math.cos(phi)), math.sin(theta)*math.sin(phi), math.cos(theta)])
+    return r
+
+def generate_array_signals(r_prime, sources, t):
+    Audio_signal = np.zeros((len(t), len(r_prime[0,:])))
+
+    for sample in range(len(t)):
+        if (sample+1 in np.arange(0,len(t),10)) or (sample == 0): # print stuff so user know how many samples that have been generated
+            print(sample+1)                                         # print stuff so user know how many samples that have been generated
+        for mic in range(len(r_prime[0,:])):
+            x_i = r_prime[0,mic]
+            y_i = r_prime[1,mic]
+            temp_signal_sample = 0
+            for source in range(len(sources)):
+                if (sources[source].get_t_start() < t[sample]) and (t[sample] < sources[source].get_t_end()):
+                    frequencies_ps = sources[source].get_frequency()
+                    theta_source = sources[source].get_theta()
+                    phi_source = sources[source].get_phi()
+                    rho_soruce = sources[source].get_rho()
+                    for freq_ind in range(len(frequencies_ps)):
+                        k = 2*math.pi*frequencies_ps[freq_ind]/config.c
+                        r_1 = np.array([x_i,y_i,0])
+                        r_2 = rho_soruce * r_vec(theta_source,phi_source)
+                        norm_coeff = np.linalg.norm(r_2-r_1)
+                        phase_offset = -k*norm_coeff
+                        element_amplitude = 1/norm_coeff
+                        temp_signal_sample += element_amplitude * math.sin(2*math.pi* frequencies_ps[freq_ind] * t[sample] + phase_offset)
+            Audio_signal[sample,mic] = temp_signal_sample
+    return Audio_signal
+
+def calc_r_prime(d):
+    half = d/2
+    r_prime = np.zeros((2, config.elements))
+    element_index = 0
+    for row in range(config.rows):
+        for col in range(config.columns*config.arrays):
+
+            r_prime[0,element_index] = col * d - config.columns * config.arrays * half + half
+            r_prime[1, element_index] = row * d - config.rows * half + half
+            element_index += 1
+
+    if config.plot_setup:
+        plt.figure()
+        plt.title('Array setup')
+        plt.scatter(r_prime[0,:], r_prime[1,:].T)
+        plt.xlim([-(d*config.columns * config.arrays/2 + d) , d*config.columns * config.arrays/2 + d])
+
+    return r_prime
+
+def main():
+    filename = generate_filename()  # generate filename
+
+    r_prime = calc_r_prime(config.distance) # calculate r_prime for the array setup
+
+    # Create and place out sources
+    source1 = Audio_source(config.f_start1, config.f_end1, config.f_res1, 
+                        config.theta_deg1, config.phi_deg1, config.away_distance, 
+                        config.t_start1, config.t_end1)
+    source2 = Audio_source(config.f_start2, config.f_end2, config.f_res2,
+                        config.theta_deg2, config.phi_deg2, config.away_distance, 
+                        config.t_start2, config.t_end2)
+    source_list = [source1, source2]
+    sources = np.array(source_list[:config.sources])
+
+    print("Creating data file: "+filename)
+    fs = config.f_sampling                      # sampling frequency in Hz
+    t_start = config.t_start                            # start time of simulation 
+    t_end = config.t_end                                # end time of simulation
+    #t_total = t_end - t_start                           # total simulation time
+    samples = config.samples
+    t = np.linspace(t_start, t_end, samples) # time vector
+
+
+    # GENERATE AUDIO SIGNAL
+    emulated_signals = generate_array_signals(r_prime, sources, t)
+    np.save('emulated_data/'+filename, emulated_signals)        # save to file
+
+    print('Audio signals generated')
+
+main()
