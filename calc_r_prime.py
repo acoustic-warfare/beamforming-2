ad89b125ae158fe211c1f6de8d4c67fa36309be7
maccke <62262159+maccke@users.noreply.github.com>
2023-07-05 16:15:56 +0200

    Add files via upload

--- calc_r_prime.py
+++ calc_r_prime.py
@@ -1,17 +1,20 @@
+import numpy as np
+import matplotlib as plt
+import config
 def calc_r_prime(d):
     half = d/2
-    r_prime = np.zeros((2, config_GS.elements))
+    r_prime = np.zeros((2, config.elements))
     element_index = 0
-    for row in range(config_GS.rows):
-        for col in range(config_GS.columns*config_GS.arrays):
-            print(str(row) + ' ' + str(col))
-            r_prime[0,element_index] = col * d - config_GS.columns * config_GS.arrays * half + half
-            r_prime[1, element_index] = row * d - config_GS.rows * half + half
+    for row in range(config.rows):
+        for col in range(config.columns*config.active_arrays):
+            #print(str(row) + ' ' + str(col))
+            r_prime[0,element_index] = col * d - config.columns * config.active_arrays * half + half
+            r_prime[1,element_index] = row * d - config.rows * half + half
             element_index += 1
 
-    plt.figure()
-    plt.title('Array setup')
-    plt.scatter(r_prime[0,:], r_prime[1,:].T)
-    plt.xlim([-(d*config_GS.columns * config_GS.arrays/2 + d) , d*config_GS.columns * config_GS.arrays/2 + d])
-
+    #plt.figure()
+    #plt.title('Array setup')
+    #plt.scatter(r_prime[0,:], r_prime[1,:].T)
+    #plt.xlim([-(d*config.columns * config.active_arrays/2 + d) , d*config.columns * config.active_arrays/2 + d])
+    #print(r_prime)
     return r_prime
